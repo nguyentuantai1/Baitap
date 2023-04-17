a91df184f86bacb04662d3ba8907c74f90a3259c
unknown <btvekinhpcuata@gmail.com>
2023-04-17 18:46:59 +0700

    BTH_21_03

--- BaiThucHanhLab2/src/Bai1.java
+++ BaiThucHanhLab2/src/Bai1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.text.DecimalFormat;
-
+//bai 1
 public class Bai1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
